1d31ab4f2191679fae9813f0245d7b88813066d9
DraganUS <usic.d.dragan@gmail.com>
2020-02-26 14:11:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#cursortyper
+#What is this? 
+
+Automatically delete a specific word in a sentence then write a new one. Ideal for titles on Landing Page
+
+#installation
+
+npm i cursortyper --save
+
+Then...
+ 
+```
+import { cursortyper } from 'cursortyper'
+
+```
+
+##options
+
+
---
+++ index.js
@@ -0,0 +1,81 @@
+var settings;
+var wormWord;
+
+function typer(settings) {
+    wormWord = document.getElementById('worm-word');
+    settings = {
+        words: wormWord.dataset.words.split(','),
+        writeTime: 2000,
+        eraseTime: 2000,
+        stopTime: 500
+    };
+    var span = document.createElement("span");
+    var text = document.createTextNode('|');
+    span.appendChild(text);
+    span.setAttribute('id','worm-word-cursor');
+    wormWord.after(span);
+
+    // KEYFRAMES
+    var keyframes = `
+    @keyframes cursorAnimation {
+        0% { opacity: 1 }
+        50% { opacity: 0 }
+        100% { opacity: 1 } 
+    }
+    `;
+    const keyframeStyle = document.createElement('style');
+    keyframeStyle.innerHTML = keyframes;
+
+    document.getElementsByTagName('head')[0].appendChild(keyframeStyle);
+
+    var wordCounter = -1;
+
+    iterateCursorTyper();
+
+    function iterateCursorTyper() {
+        if (settings.words.length === wordCounter + 1) {
+            wordCounter = 0;
+        } else {
+            wordCounter++;
+        }
+        wordWriter(settings.words[wordCounter]);
+    }
+
+    function wordWriter(word) {
+        document.getElementById('worm-word-cursor').style.animation = "cursorAnimation 1s ease-in-out infinite";
+
+        var i;
+        for (var i = 0; i < word.length; i++) {
+            letterWriter(i, word.charAt(i));
+        }
+        setTimeout(()=>{
+            wordEraser(i);
+        }, (i+1) * 500);
+    }
+
+    function letterWriter(i, letter) {
+        setTimeout(() => {
+            wormWord.innerHTML += letter;
+        }, i * 500)
+    }
+
+    function wordEraser(d) {
+        document.getElementById('worm-word-cursor').style.animation = "";
+        for (var j = 0; j < wormWord.innerHTML.length; j++) {
+            letterEraser(j)
+        }
+        setTimeout(()=>{
+            iterateCursorTyper();
+        }, (d+1) * 500)
+
+    }
+
+    function letterEraser(j) {
+        setTimeout(() => {
+            wormWord.innerHTML =
+                wormWord.innerHTML.substring(0, wormWord.innerHTML.length-1);
+        }, j * 500);
+    }
+
+}
+module.exports.typer = typer;
